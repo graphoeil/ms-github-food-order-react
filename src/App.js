584dfe243de23e304a446dedae2ef66a2de81b24
graphoeil <graphoeil@gmail.com>
2022-11-10 12:24:23 +0100

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import React, { useState } from "react";
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Modal from "./components/UI/Modal";
 
 function App() {
-  return (
-    <CartProvider>
-      <Header />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
-  );
-}
+
+	// Show cart ?
+	const [showCart, setShowCart] = useState(false);
+
+	// Show / hide cart
+	const showCartHandler = () => {
+		setShowCart(true);
+	};
+	const hideCartHandler = () => {
+		setShowCart(false);
+	};
+
+	// Return
+	return (
+		<CartProvider>
+			{
+				showCart && <div>Cart</div>
+			}
+			<Header onShowCart={ showCartHandler }/>
+			<main>
+				<Meals />
+			</main>
+		</CartProvider>
+	);
+};
 
 export default App;
